82907bf1b38b2ac9f465c9bc8d2aea72feaecb69
Kristof <mrmaderat1@gmail.com>
2023-11-14 22:59:57 +0200

    bump version and localizations

--- src/main/java/union/App.java
+++ src/main/java/union/App.java
@@ -143,7 +143,7 @@ public class App {
 			.useHelpBuilder(false)
 			.setScheduleExecutor(scheduledExecutor)
 			.setStatus(OnlineStatus.ONLINE)
-			.setActivity(Activity.customStatus("/help"))
+			.setActivity(Activity.customStatus(">>>  /help  <<<"))
 			.addSlashCommands(
 				// guild
 				new SetupCmd(this),

d88ad19f3c07b1c2eacfb5eb8f763b74faab5bd0
Kristof <mrmaderat1@gmail.com>
2025-02-16 10:18:52 +0200

    Fix level up logs

--- src/main/java/union/utils/logs/LogEmbedUtil.java
+++ src/main/java/union/utils/logs/LogEmbedUtil.java
@@ -188,11 +188,6 @@ public class LogEmbedUtil {
 			return this;
 		}
 
-		public LogEmbedBuilder setTitleText(String text) {
-			builder.setTitle(text);
-			return this;
-		}
-
 		public LogEmbedBuilder setDescription(String text) {
 			builder.setDescription(text);
 			return this;
@@ -1262,7 +1257,7 @@ public class LogEmbedUtil {
 	public MessageEmbed levelUp(DiscordLocale locale, Member member, int level, ExpType expType) {
 		return new LogEmbedBuilder(locale, GREEN_DARK)
 			.setHeaderIcon(LogEvent.LEVEL_UP, member.getEffectiveAvatarUrl(), member.getUser().getName())
-			.setTitleText(lu.getLocalizedRandom(locale, "logger.levels.msg_random")
+			.setDescription(lu.getLocalizedRandom(locale, "logger.levels.msg_random")
 				.replace("{user}", "<@!%s>".formatted(member.getIdLong()))
 				.replace("{level}", String.valueOf(level))
 				.replace("{type}", expType.equals(ExpType.TEXT) ? "\uD83D\uDCAC" : "\uD83C\uDF99️")

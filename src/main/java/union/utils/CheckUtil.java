f35e537bc591972f96492492f83e3a281840aa48
Kristof <mrmaderat1@gmail.com>
2025-05-02 19:43:58 +0300

    wip

--- src/main/java/union/utils/CheckUtil.java
+++ src/main/java/union/utils/CheckUtil.java
@@ -69,6 +69,19 @@ public class CheckUtil {
 			.orElse(CmdAccessLevel.ALL);
 	}
 
+	public boolean isOperatorPlus(Guild guild, UserSnowflake user) {
+		// Is bot developer
+		if (isDeveloper(user) || isBotOwner(user))
+			return true;
+
+		// Is guild's owner
+		if (guild.getOwnerIdLong() == user.getIdLong())
+			return true;
+
+		// Check if is operator
+        return bot.getDBUtil().access.isOperator(guild.getIdLong(), user.getIdLong());
+    }
+
 	public boolean hasHigherAccess(Member who, Member than) {
 		return getAccessLevel(who).isHigherThan(getAccessLevel(than));
 	}
